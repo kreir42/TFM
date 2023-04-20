e5201e6c72ef242a56c8a7a5a3028d7347da14ae
kreir42 <gortle97@gmail.com>
2023-04-20 12:55:57 +0200

    psd plot

--- pulsed.cxx
+++ pulsed.cxx
@@ -5,10 +5,12 @@ void pulsed_per_file(char filepath[500]){
 	auto monster = d.Filter("Channel==4");
 	Double_t max_tof = monster.Max("tof").GetValue();
 	auto tof_plot = monster.Histo1D({"tof_plot", ";ToF;Counts", 1000, 0, max_tof}, "tof");
+	auto psd_plot = monster.Histo1D({"psd_plot", ";psd;Counts", 1000, 0, 1}, "psd");
 	auto tof_id_plot = monster.Histo2D({"tof_id_plot", ";ToF;PSD;Counts", 1000, 0, max_tof, 25, 0, 1}, "tof", "psd");
 	auto energy_id_plot = monster.Histo2D({"energy_id_plot", ";Energy;PSD;Counts", 4096, 0, 4096, 25, 0, 1}, "Energy", "psd");
 
 	tof_plot->Write("", TObject::kOverwrite);
+	psd_plot->Write("", TObject::kOverwrite);
 	tof_id_plot->Write("", TObject::kOverwrite);
 	energy_id_plot->Write("", TObject::kOverwrite);
 	DisableImplicitMT();
